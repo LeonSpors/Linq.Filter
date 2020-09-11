40218022386e8eff39dd9c1c9d271abaffa16245
Leon Spors <15744335+halfbax@users.noreply.github.com>
2020-09-11 20:43:54 +0200

    Changed folder structure

---
+++ Spors.Linq.Filter.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spors.Linq.Filter.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
